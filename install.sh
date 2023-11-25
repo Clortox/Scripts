a742810d673653c14efe74874562455ec0352994
Tyler Perkins <tyler@clortox.com>
2023-11-25 18:18:54 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+INSTALL_LOCATION="$HOME/.local/bin"
+
+echo "Installing scripts to $INSTALL_LOCATION"
+
+cp -v ./bkmark      $INSTALL_LOCATION
+cp -v ./fix_screens $INSTALL_LOCATION
+cp -v ./mount_drive $INSTALL_LOCATION
+cp -v ./passmenu    $INSTALL_LOCATION
+cp -v ./pipes.sh    $INSTALL_LOCATION
+
+if [[ "$PATH:" != *":$HOME/.local/bin:"* ]]; then
+    echo "The install location ($INSTALL_LOCATION), is not in your PATH! Please add it to you path."
+fi
